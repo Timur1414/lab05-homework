0c5d3e2ca8049a6d5ede956f25f6ee3e2a08cb8b
Timur <tima.temirov.2005@mail.ru>
2024-02-23 11:45:44 -0500

    source + tests

---
+++ tests/test1.cpp
@@ -0,0 +1,22 @@
+#include <banking.h>
+#include <gtest/gtest.h>
+
+TEST(Banking_tests, Transaction1) {
+    int last_id = 0;
+    Account a(last_id, 10);
+    Account b(last_id, 50);
+    Transaction t1(&a, &b, 10);
+    t1.exec();
+    EXPECT_EQ(a.get_money(), 0);
+    EXPECT_EQ(b.get_money(), 60);
+}
+
+TEST(Banking_tests, Transaction2) {
+    int last_id = 0;
+    Account a(last_id, 10);
+    Account b(last_id, 50);
+    Transaction t1(&a, &b, 50);
+    t1.exec();
+    EXPECT_EQ(a.get_money(), 10);
+    EXPECT_EQ(b.get_money(), 50);
+}
