0d2928cca62b1760158e38debd9473d3d5444e75
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-03-27 21:25:11 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,22 +2,23 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-class Transaction_Interface {
+class Acc_Interface {
 public:
-    virtual ~Transaction_Interface() = default;
-    virtual bool can_exec() = 0;
-    virtual void exec() {
-        if (!can_exec())
-            return;
-        // code
+    virtual ~Acc_Interface() = default;
+    virtual void set_money(int) = 0;
+};
+
+class Acc : public Acc_Interface {
+public:
+    void set_money(int money) {
+	// code
     }
 };
 
-class Transaction_Mock : public Transaction_Interface {
+class Acc_Mock : public Acc_Interface {
 public:
-    ~Transaction_Mock() override  = default;
-    MOCK_METHOD0(can_exec, bool());
-    MOCK_METHOD0(exec, void());
+    ~Acc_Mock() override  = default;
+    MOCK_METHOD1(set_money, void(int));
 };
 
 
@@ -43,10 +44,9 @@ TEST(Banking_tests, Transaction2) {
 
 TEST(Banking_tests, Mock) {
     int last_id = 0;
-    Account a(last_id, 10);
-    Account b(last_id, 50);
-    Transaction_Mock t1;
-    EXPECT_CALL(t1, can_exec());
-    t1.exec();
-    EXPECT_CALL(t1, exec());    
+    Acc_Mock a;
+    Acc_Mock b;
+    
+    EXPECT_CALL(a, set_money(50)).Times(1);
+    a.set_money(50);
 }
